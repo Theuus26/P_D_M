29aeb3c0dca2924d8f58bbb984d016b789f3f248
Your Name <pachecomatheus45@gmail.com>
2023-04-28 04:05:43 -0300

    maters

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Avaliação_PDM',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
